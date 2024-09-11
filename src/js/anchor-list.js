d9860eb307c55934f77a785c9551c16b42ed8a85
johnhoysa <johnhoysa@gmail.com>
2024-09-11 08:30:00 -0500

    prettier formatting

--- src/js/anchor-list.js
+++ src/js/anchor-list.js
@@ -1,26 +1,26 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-      const scrollId = this.getAttribute('href').substring(1);
-      const scrollElement = document.getElementById(scrollId);
-      const headerOffset = 116;
-      const scrollPosition = scrollElement.getBoundingClientRect().top;
-      const offsetPosition = scrollPosition + window.pageYOffset - headerOffset;
+document.querySelectorAll('nav a').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const scrollId = this.getAttribute('href').substring(1);
+    const scrollElement = document.getElementById(scrollId);
+    const headerOffset = 116;
+    const scrollPosition = scrollElement.getBoundingClientRect().top;
+    const offsetPosition = scrollPosition + window.pageYOffset - headerOffset;
 
-      window.scrollTo({
-          top: offsetPosition,
-          behavior: 'smooth'
-      });
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: 'smooth'
+    });
   });
 });
 
 // Highlight active anchor link based on the section in view
 // Get all anchor links
-const links = document.querySelectorAll("nav div ul li a");
+const links = document.querySelectorAll('nav div ul li a');
 
 //get sections
-const sections = document.querySelectorAll("div[id]");
-console.log('sections', sections)
+const sections = document.querySelectorAll('div[id]');
+console.log('sections', sections);
 // Extract the id values into an array this is used to observe the elements
 // These ids should match the href value of the anchor links
 const ids = Array.from(sections).map((section) => section.id);
@@ -31,13 +31,13 @@ const callback = (entries, observer) => {
     if (entry.isIntersecting) {
       // remove class from all links
       links.forEach((link) => {
-        link.classList.remove("border-b", "pb-1");
+        link.classList.remove('border-b', 'pb-1');
       });
       // add class to the current link
       document
         .querySelector(`nav ul li a[href="#${entry.target.id}"]`)
-        .classList.add("border-b", "pb-1");
-    // no need for an else statement as the class is removed from all links
+        .classList.add('border-b', 'pb-1');
+      // no need for an else statement as the class is removed from all links
     }
   });
 };
@@ -46,8 +46,8 @@ const callback = (entries, observer) => {
 // This is where I can add additional options in the future
 const observer = new IntersectionObserver(callback, {
   root: null,
-  rootMargin: "0px",
-  threshold: 0, 
+  rootMargin: '0px',
+  threshold: 0
 });
 
 // Loop through the sections and observe each one by it's Id
