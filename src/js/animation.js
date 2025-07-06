d9077db1e344f7493686c3191e48ca5f5f986627
johnhoysa <johnhoysa@gmail.com>
2025-07-06 13:35:37 -0500

    social icons footer now have hoover

--- src/js/animation.js
+++ src/js/animation.js
@@ -5,8 +5,7 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 // Register plugins here
 gsap.registerPlugin(ScrollTrigger);
 
-// Nav Items
-
+// Nav Items Hover
 document.addEventListener('DOMContentLoaded', () => {
   const navItems = document.querySelectorAll('#navItems li');
 
@@ -36,10 +35,37 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-// Social Items
-
+// Social Items Header Hover
 document.addEventListener('DOMContentLoaded', () => {
   const navSocialItems = document.querySelectorAll('#navSocial li');
+
+  navSocialItems.forEach((item) => {
+    const circleBG = item;
+
+    // Create a timeline paused by default
+    const tl = gsap.timeline({ paused: true });
+    tl.to(circleBG, {
+      background: '#EE684A',
+      duration: 0.5,
+      ease: 'ease.inOut'
+    });
+
+    item.addEventListener('mouseenter', () => {
+      tl.play();
+    });
+
+    item.addEventListener('mouseleave', () => {
+      tl.reverse();
+    });
+    item.addEventListener('click', () => {
+      tl.play();
+    });
+  });
+});
+
+// Social Items Footer Hover
+document.addEventListener('DOMContentLoaded', () => {
+  const navSocialItems = document.querySelectorAll('#navSocialFooter li');
 
   navSocialItems.forEach((item) => {
     const circleBG = item;
