8a9dec438049c0bdef0d4627cfd461a390467803
johnhoysa <johnhoysa@gmail.com>
2025-07-11 11:07:18 -0500

    animate text no longer needed

--- src/main.js
+++ src/main.js
@@ -4,6 +4,5 @@ import './css/resources/fonts.css';
 // JS 2
 import './js/anchor-list.js';
 import './js/animation.js';
-import './js/animate-text.js';
 import './js/lazy-load.js';
 import './js/navigation.js';
