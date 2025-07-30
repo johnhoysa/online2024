338c7913d7639070292471dcc5176d7cdf915b0f
johnHoysa <john.hoysa@gmail.com>
2025-07-30 09:16:37 -0500

    updated loading text to white

--- src/js/smore/rick-morty.js
+++ src/js/smore/rick-morty.js
@@ -106,12 +106,12 @@ if (getData) {
         results.innerHTML = `
   <h2 class="text-center mt-20 text-2xl text-white w-full lg:w-3/5 mx-auto">
   Okay, so, get this, multiverse stuff. Which version of me, I mean ${firstName} is, uh, your favorite? No wrong answers! Except maybe some.</h2>
-  <div class="flex flex-wrap gap-6 justify-center mt-8" id="relatedList" aria-live="polite">Loading...</div>`;
+  <div class="flex text-white flex-wrap gap-6 justify-center mt-8" id="relatedList" aria-live="polite">Loading...</div>`;
       } else {
         results.innerHTML = `
   <h2 class="text-center pt-32 text-2xl text-white w-full lg:w-3/5 mx-auto">
   Okay, so, get this, multiverse stuff. Which version of the ${firstName} is, uh, your favorite? No wrong answers! Except maybe some.</h2>
-  <div class="flex flex-wrap gap-6 justify-center mt-8" id="relatedList" aria-live="polite">Loading...</div>`;
+  <div class="flex text-white flex-wrap gap-6 justify-center mt-8" id="relatedList" aria-live="polite">Loading...</div>`;
       }
 
       // lets fetch content related to the first name
